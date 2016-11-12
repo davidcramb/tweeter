b040c47ca477706267c78958c10f26ae999b368f
David Cramb <david.cramb@gmail.com>
2016-11-12 09:38:02 -0600

    fixed angular issue

--- Tweeter/Scripts/app.js
+++ Tweeter/Scripts/app.js
@@ -1,2 +1,14 @@
-﻿var app = angular.module("tweeter", []);
-console.log("hi");
+﻿var app = angular.module("Tweeter", []);
+
+app.controller("FormCtrl", ["$scope", "$http", function ($scope, $http) {
+
+    $scope.welcome = 'hi';
+
+    $scope.formSubmit = (event, user) => {
+        event.preventDefault();
+        var data = $("form").serialize();
+        $http.post('/Account/Register', data)
+        .then(console.log(data));
+    }
+
+}]);
